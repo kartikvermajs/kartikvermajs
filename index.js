b6d625a0efb741dcd31526b820231e54779c3784
Kartik Verma <kartikverma88273@gmail.com>
2026-03-18 19:18:00 +0530

    final commit

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 import jsonfile from "jsonfile";
 import moment from "moment";
+import simpleGit from "simple-git";
 
 const path = "./data.json";
-const date = moment().format();
 
-const data = {
-  data: date,
+const makeCommits = (n) => {
+  if (n === 0) return simpleGit().push();
+
+  const x = Math.floor(Math.random() * 55);
+  const y = Math.floor(Math.random() * 7);
+
+  const date = moment()
+    .subtract(1, "y")
+    .add(1, "d")
+    .add(x, "w")
+    .add(y, "d")
+    .format();
+
+  const data = { date };
+
+  console.log(date);
+
+  jsonfile.writeFile(path, data, () => {
+    simpleGit()
+      .add([path])
+      .commit(date, { "--date": date }, makeCommits.bind(this, --n));
+  });
 };
 
-jsonfile.writeFile(path, data);
+makeCommits(100);
